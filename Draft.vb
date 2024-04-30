e78b9eb669dda42c2aef22a75035c7a98287641f
Julian-Panditharatne <julianpanditharatne@hotmail.com>
2024-04-30 17:06:02 -0400

    Started writing the draft

--- Draft.vb
+++ Draft.vb
@@ -1,3 +1,10 @@
 Sub QuarterlyReport():
-    
+    Dim ws As Worksheet
+    ' Got the code for looping through worksheets from https://www.goskills.com/Excel/Resources/VBA-code-library#Worksheetcodes
+    For Each ws In ActiveWorkbook.Worksheet
+        Dim entries As Long
+        entries = ws.Rows.Count ' Got the code for counting the rows in a worksheet from https://www.homeandlearn.org/other_excel_vba_variable_types.html
+        
+    Next ws
+
 End Sub
