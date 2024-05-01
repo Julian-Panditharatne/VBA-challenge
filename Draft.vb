59005bab6759e60e73012689bb3d37210cdd382d
Julian-Panditharatne <julianpanditharatne@hotmail.com>
2024-05-01 19:20:19 -0400

    Drafted a rough sketch of nested loop procedure

--- Draft.vb
+++ Draft.vb
@@ -23,6 +23,30 @@ Sub Quarter1Report():
         Dim num_entries As Long
         num_entries = ws.Rows.Count ' Got the code for counting the rows in a worksheet from https://www.homeandlearn.org/other_excel_vba_variable_types.html
         
+
+
+        For i = 2 To num_entries
+            Dim token As Variant ' The name of the token.
+            Dim token_open As Integer 'The opening price of the token at the start of quarter(1).
+            Dim token_close As Integer 'The closing price of the token at the end of quarter(1).
+            Dim date As Date 
+
+            date = Cells(i, 2).Value
+
+            ' If Then condition that keeps skipping entries until loop reaches the right quarter(1).
+
+            token = Cells(i, 1).Value
+            token_open = Cells(i, 3).Value
+            token_close = Cells(i, 6).Value
+
+            For j = i+1 To num_entries
+                If Cells(j, 1).Value = token Then ' Add conditions to check whether entry is in the right quarter(1).
+
+                End If 
+            Next j
+
+        Next i
+
     Next ws
 
 End Sub
